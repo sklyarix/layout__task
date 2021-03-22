f8a7ee374ba71211d432baea0438e6188efc83c5
sklyarix <sklyar.sasha.26@yandex.ru>
2021-03-22 14:12:16 +0400

    Not__animation

---
+++ js/index.js
@@ -0,0 +1,68 @@
+const countEuro = () => {
+    {/*euro = 88.72*/}
+    
+    document.getElementById("7990").innerText = "90,06 €";
+    document.getElementById("9990").innerText = "112,60 €";
+    document.getElementById("24990").innerText = "281,68 €";
+
+    document.getElementById("890").innerText = "10,10 €";
+    document.getElementById("499").innerText = "5,66 €";
+
+    document.getElementById("3900").innerText = "44,26 €";
+    document.getElementById("2900").innerText = "32,91 €";
+
+    document.getElementById("11900").innerText = "135,06 €";
+    document.getElementById("9900").innerText = "112,36 €";
+}
+const countDollars = () => {
+    {/*dollar = 74,25*/}
+    
+    document.getElementById("7990").innerText = "107,61 $";
+    document.getElementById("9990").innerText = "134,54	$";
+    document.getElementById("24990").innerText = "336,56 $";
+
+    document.getElementById("890").innerText = "12,00 $";
+    document.getElementById("499").innerText = "6,73 $";
+
+    document.getElementById("3900").innerText = "52,58 $";
+    document.getElementById("2900").innerText = "39,10 $";
+
+    document.getElementById("11900").innerText = "160,44 $";
+    document.getElementById("9900").innerText = "133,48 $";
+    
+}
+const countRuble = () => {
+    {/**/}
+    
+    document.getElementById("7990").innerText = "7990 ₽";
+    document.getElementById("9990").innerText = "9990 ₽";
+    document.getElementById("24990").innerText = "24990 ₽";
+
+    document.getElementById("890").innerText = "890 ₽";
+    document.getElementById("499").innerText = "499 ₽";
+
+    document.getElementById("3900").innerText = "3900 ₽";
+    document.getElementById("2900").innerText = "2900 ₽";
+
+    document.getElementById("11900").innerText = "11900 ₽";
+    document.getElementById("9900").innerText = "9900 ₽";
+}
+const countGrivny = () => {
+    {/*Grivny = 2,68*/}
+    
+    document.getElementById("7990").innerText = "2982,62 ₴";
+    document.getElementById("9990").innerText = "3729,21 ₴";
+    document.getElementById("24990").innerText = "9328,62 ₴";
+
+    document.getElementById("890").innerText = "332,60 ₴";
+    document.getElementById("499").innerText = "186,48 ₴";
+
+    document.getElementById("3900").innerText = "1457,45 ₴";
+    document.getElementById("2900").innerText = "1083,74 ₴";
+
+    document.getElementById("11900").innerText = "4447,08 ₴";
+    document.getElementById("9900").innerText = "3699,67 ₴";
+    
+}
+
+
